6b26220840079be7b2639e2592860ee5cd8de77b
kieutrangdev <104716412+kieutrangdev@users.noreply.github.com>
2022-12-09 18:55:33 +0700

    design-header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <button>Test</button>
+   <>
+    <Header/>
+   </>
   );
 }
 
