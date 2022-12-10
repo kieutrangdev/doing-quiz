4c2bbceba0395fbfc3847c703981cc03a8c636cb
kieutrangdev <104716412+kieutrangdev@users.noreply.github.com>
2022-12-10 18:26:13 +0700

    create-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
-
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+const App = () => {
   return (
-   <>
-    <Header/>
-   </>
+    <>
+      <Header />
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users">User page</Link>
+          </button>
+          <button>
+            <Link to="/admin">Admin page</Link>
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/admin/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = (props) => {
+  return (
+    <div>
+      Admin component
+    </div>
+  )
+}
+
+export default Admin
